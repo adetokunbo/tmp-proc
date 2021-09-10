2565311b343a9184d5aa5da27587d0ac8019a25b
Tim Emiola <adetokunbo@users.noreply.github.com>
2021-09-10 12:35:09 +0900

    Extend the HttpBin test fixture
    - to better detect compiler errors related to type-level selection etc

--- tmp-proc/test/Test/HttpBin.hs
+++ tmp-proc/test/Test/HttpBin.hs
@@ -6,19 +6,21 @@
 {-# LANGUAGE TypeFamilies        #-}
 module Test.HttpBin where
 
-import           Control.Exception     (catch)
-import qualified Data.ByteString.Char8 as C8
-import           Data.List             (foldl')
-import           Data.Text             (Text)
-import qualified Data.Text             as Text
+import           Control.Exception        (catch)
+import qualified Data.ByteString.Char8    as C8
+import           Data.List                (foldl')
+import           Data.Text                (Text)
+import qualified Data.Text                as Text
 import           Network.HTTP.Req
 
-import           System.TmpProc.Docker (HList (..), HostIpAddress, Proc (..),
-                                        ProcHandle (..), SvcURI, startupAll)
+import           System.TmpProc.Docker    (HList (..), HostIpAddress, Proc (..),
+                                           Proc2Handle, ProcHandle (..), SvcURI,
+                                           startupAll)
+import           System.TmpProc.TypeLevel ((%:))
 
 
-setupHandles :: IO (HList '[ProcHandle HttpBinTest])
-setupHandles = startupAll $ HttpBinTest `HCons` HNil
+setupHandles :: IO (HList (Proc2Handle '[HttpBinTest, HttpBinTest2, HttpBinTest3]))
+setupHandles = startupAll $ HttpBinTest %: HttpBinTest2 %: HttpBinTest3 %: HNil
 
 
 {-| A data type representing a connection to the HttpBin server. -}
@@ -35,13 +37,54 @@ instance Proc HttpBinTest where
   ping = ping'
 
 
+{-| Another data type representing a connection to the HttpBin server.
+
+
+Allows the test tmp process list used to have multiple entries, to better
+detect any compiler errors in the tmp-proc library.
+
+-}
+data HttpBinTest2 = HttpBinTest2
+
+{-| Run HttpBin as temporary process.  -}
+instance Proc HttpBinTest2 where
+  type Image HttpBinTest2 = "kennethreitz/httpbin"
+  type Name HttpBinTest2 = "http-bin-test-2"
+
+  uriOf = mkUri'
+  runArgs = []
+  reset _ = pure ()
+  ping = ping'
+
+
+{-| Yet another data type representing a connection to the HttpBin server.
+
+
+Allows the test tmp process list used to have multiple entries, to better
+detect any compiler errors in the tmp-proc library.
+
+-}
+data HttpBinTest3 = HttpBinTest3
+
+{-| Run HttpBin as temporary process.  -}
+instance Proc HttpBinTest3 where
+  type Image HttpBinTest3 = "kennethreitz/httpbin"
+  type Name HttpBinTest3 = "http-bin-test-3"
+
+  uriOf = mkUri'
+  runArgs = []
+  reset _ = pure ()
+  ping = ping'
+
+
+
 {-| Make a uri access the http-bin server. -}
 mkUri' :: HostIpAddress -> SvcURI
 mkUri' ip = "http://" <> C8.pack (Text.unpack ip) <> "/"
 
 
 {-| Does nothing, httpBin is stateless. -}
-ping' :: ProcHandle HttpBinTest -> IO ()
+ping' :: ProcHandle a -> IO ()
 ping' handle = do
   let catchHttp x = x `catch` (\(_ :: HttpException) ->
                                   fail "tmp proc:httpbin:ping failed")
@@ -52,12 +95,12 @@ ping' handle = do
 
 
 -- | Determine the status from a Get on localhost.
-handleGet :: ProcHandle HttpBinTest -> Text -> IO Int
+handleGet :: ProcHandle a -> Text -> IO Int
 handleGet handle urlPath = runReq defaultHttpConfig $ do
   r <- req GET (handleUrl handle urlPath) NoReqBody ignoreResponse $ mempty
   return $ responseStatusCode r
 
 
-handleUrl :: ProcHandle HttpBinTest -> Text -> Url 'Http
+handleUrl :: ProcHandle a -> Text -> Url 'Http
 handleUrl handle urlPath = foldl' (/:) (http $ hAddr handle)
   $ Text.splitOn "/" $ Text.dropWhile (== '/') urlPath
