92bb9df07e4c07546136337822c3cd1c74acd2f5
Tim Emiola <adetokunbo@users.noreply.github.com>
2021-09-20 21:24:07 +0900

    Make a copy of Test.Hspec.TmpProc in tmp-proc/test
    .. to avoid an awkward dependency between tmp-proc and hspec-tmp-proc

--- tmp-proc/test/Spec.hs
+++ tmp-proc/test/Spec.hs
@@ -8,13 +8,10 @@ import           System.IO
 import qualified Test.System.TmpProc.HttpBinSpec as HB
 import qualified Test.System.TmpProc.WarpSpec    as W3
 
-import           System.TmpProc.Docker     (hasDocker)
-
 main :: IO ()
 main = do
   hSetBuffering stdin NoBuffering
   hSetBuffering stdout NoBuffering
-  noDocker <- not <$> hasDocker
   hspec $ do
-    HB.spec noDocker
-    W3.spec noDocker
+    HB.spec
+    W3.spec
