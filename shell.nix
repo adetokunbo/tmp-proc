6bdebacf401da560e9187319fd24877de1f801da
Tim Emiola <adetokunbo@users.noreply.github.com>
2023-07-14 18:19:00 +0900

    Remove tools from the nix shell
    - to ensure that the nix-shell dev environment can be used
    
    - ideally nothing would be removed, but there are various bugs in haskell.nix
    ...

--- shell.nix
+++ shell.nix
@@ -2,22 +2,15 @@ let
   project = import ./default.nix;
 in
   project.shellFor {
-    # Builds a Hoogle documentation index of all dependencies,
-    # and provides a "hoogle" command to search the index.
-    withHoogle = true;
 
     # Some common tools can be added with the `tools` argument
     tools = {
       cabal = "latest";
-      haskell-language-server = "latest";
+      haskell-language-server = {
+        version = "latest";
+        src = project.pkgs.haskell-nix.sources."hls-2.0";
+      };
       ghcid = "latest";
-      fourmolu = "latest";
       cabal-fmt = "latest";
-      hlint = "3.4.1";  # using a specific version, at this nix pin latest does not build
     };
-    # See overlays/tools.nix for more details
-
-    # Prevents cabal from choosing alternate plans, so that
-    # *all* dependencies are provided by Nix.
-    exactDeps = true;
   }
