0441ffef21bc18739281324a1104b79fe94ce4fc
Tim Emiola <adetokunbo@users.noreply.github.com>
2023-07-15 19:36:54 +0900

    Update System.TmpProc.WarpSpeUpdate System.TmpProc.WarpSpec

--- tmp-proc/test/Test/System/TmpProc/WarpSpec.hs
+++ tmp-proc/test/Test/System/TmpProc/WarpSpec.hs
@@ -1,32 +1,42 @@
-{-# LANGUAGE DataKinds           #-}
-{-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE GADTs               #-}
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE PolyKinds           #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications    #-}
-{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Test.System.TmpProc.WarpSpec where
 
-import           Test.Hspec
-
-import           Control.Exception     (catch)
-import           Data.Proxy            (Proxy (..))
-import           Data.Text             (Text)
-import           Network.HTTP.Types    (status200, status400)
+import Control.Exception (catch)
+import Data.Proxy (Proxy (..))
+import Data.Text (Text)
 import qualified Network.HTTP.Client as HC
-import           Network.Wai           (Application, pathInfo, responseLBS)
-
-import           System.TmpProc.Docker (HList (..), HandlesOf, Pinged (..),
-                                        ProcHandle, handleOf, ixPing)
-import           System.TmpProc.Warp   (ServerHandle, handles, runServer,
-                                        runTLSServer, serverPort, shutdown,
-                                        testWithApplication,
-                                        testWithTLSApplication)
-import           Test.Hspec.TmpProc    (tdescribe)
-import           Test.HttpBin
-import           Test.SimpleServer     (defaultTLSSettings, statusOfGet,
-                                        statusOfGet')
+import Network.HTTP.Types (status200, status400)
+import Network.Wai (Application, pathInfo, responseLBS)
+import System.TmpProc.Docker (
+  HList (..),
+  HandlesOf,
+  Pinged (..),
+  ProcHandle,
+  handleOf,
+  ixPing,
+ )
+import System.TmpProc.Warp (
+  ServerHandle,
+  handles,
+  runServer,
+  runTLSServer,
+  serverPort,
+  shutdown,
+  testWithApplication,
+  testWithTLSApplication,
+ )
+import Test.Hspec
+import Test.Hspec.TmpProc (tdescribe)
+import Test.HttpBin
+import Test.SimpleServer (statusOfGet)
 
 
 spec :: Spec
@@ -39,9 +49,10 @@ testProcs = HttpBinTest `HCons` HNil
 
 
 testApp :: HandlesOf '[HttpBinTest] -> IO Application
-testApp hs = mkTestApp'
-  (pingOrFail $ handleOf @"http-bin-test" Proxy hs)
-  (pingOrFail $ handleOf @"http-bin-test" Proxy hs)
+testApp hs =
+  mkTestApp'
+    (pingOrFail $ handleOf @"http-bin-test" Proxy hs)
+    (pingOrFail $ handleOf @"http-bin-test" Proxy hs)
 
 
 setupBeforeAll :: IO (ServerHandle '[HttpBinTest])
@@ -53,7 +64,6 @@ setupBeforeAll = runServer testProcs testApp
 --   tls <- defaultTLSSettings
 --   runTLSServer tls testProcs testApp
 
-
 suffixAround, suffixBeforeAll, prefixHttp, prefixHttps :: String
 suffixAround = " when the server is restarted for each test"
 suffixBeforeAll = " when the server starts beforeAll tests"
@@ -64,16 +74,17 @@ prefixHttps = "Warp+HTTPS:"
 beforeAllSpec :: Spec
 beforeAllSpec = do
   checkBeforeAll prefixHttp setupBeforeAll statusOfGet
-  -- checkBeforeAll prefixHttps setupBeforeAllTls statusOfGet'
 
 
-checkBeforeAll
-  :: String
-  -> IO (ServerHandle '[HttpBinTest])
-  -> (Int -> Text -> IO Int) -> Spec
-checkBeforeAll descPrefix setup getter =  beforeAll setup $ afterAll shutdown $ do
+-- checkBeforeAll prefixHttps setupBeforeAllTls statusOfGet'
+
+checkBeforeAll ::
+  String ->
+  IO (ServerHandle '[HttpBinTest]) ->
+  (Int -> Text -> IO Int) ->
+  Spec
+checkBeforeAll descPrefix setup getter = beforeAll setup $ afterAll shutdown $ do
   describe (descPrefix ++ suffixBeforeAll) $ do
-
     it "should ping the proc handle" $ \sh ->
       ixPing @"http-bin-test" Proxy (handles sh) `shouldReturn` OK
 
@@ -90,34 +101,34 @@ setupAround = testWithApplication testProcs testApp
 --   tls <- defaultTLSSettings
 --   testWithTLSApplication tls testProcs testApp cont
 
-
 aroundSpec :: Spec
 aroundSpec = do
   checkEachTest prefixHttp setupAround statusOfGet
-  -- checkEachTest prefixHttps setupAroundTls statusOfGet'
 
 
-checkEachTest
-  :: String
-  -> (ActionWith (HandlesOf '[HttpBinTest], Int) -> IO ())
-  -> (Int -> Text -> IO Int)
-  -> Spec
+-- checkEachTest prefixHttps setupAroundTls statusOfGet'
+
+checkEachTest ::
+  String ->
+  (ActionWith (HandlesOf '[HttpBinTest], Int) -> IO ()) ->
+  (Int -> Text -> IO Int) ->
+  Spec
 checkEachTest descPrefix setup getter = around setup $ do
   describe (descPrefix ++ suffixAround) $ do
-
     it "should ping the proc handle" $ \(h, _) ->
-        ixPing @"http-bin-test" Proxy h `shouldReturn` OK
+      ixPing @"http-bin-test" Proxy h `shouldReturn` OK
 
     it "should invoke the warp server via its port" $ \(_, p) ->
-        getter p "test" `shouldReturn` 200
+      getter p "test" `shouldReturn` 200
 
 
--- | A WAI app that triggers an action on a TmpProc dependency on /test, and
--- responds to health checks on /health.
-mkTestApp'
-  :: IO ()
-  -> IO ()
-  -> IO Application
+{- | A WAI app that triggers an action on a TmpProc dependency on /test, and
+responds to health checks on /health.
+-}
+mkTestApp' ::
+  IO () ->
+  IO () ->
+  IO Application
 mkTestApp' onStart onTest = onStart >> pure app
   where
     app rq respond
@@ -133,9 +144,13 @@ mkTestApp' onStart onTest = onStart >> pure app
 
 pingOrFail :: ProcHandle a -> IO ()
 pingOrFail handle = do
-  let catchHttp x = x `catch` (\(_ :: HC.HttpException) ->
-                                  fail "tmp proc:httpbin:ping failed")
+  let catchHttp x =
+        x
+          `catch` ( \(_ :: HC.HttpException) ->
+                      fail "tmp proc:httpbin:ping failed"
+                  )
   catchHttp $ do
     gotStatus <- handleGet handle "/status/200"
-    if (gotStatus == 200) then pure () else
-      fail "tmp proc:httpbin:incorrect ping status"
+    if (gotStatus == 200)
+      then pure ()
+      else fail "tmp proc:httpbin:incorrect ping status"
