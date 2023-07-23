97cf029d6d292ec733b9940acb51c23aef49637a
Tim Emiola <adetokunbo@users.noreply.github.com>
2023-07-23 20:35:51 +0900

    Use more pings when bringing up ZipKin

--- tmp-proc-zipkin/src/System/TmpProc/Docker/Zipkin.hs
+++ tmp-proc-zipkin/src/System/TmpProc/Docker/Zipkin.hs
@@ -1,12 +1,13 @@
-{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE NamedFieldPuns        #-}
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TypeApplications      #-}
-{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# OPTIONS_HADDOCK prune not-home #-}
-{-|
+
+{- |
 Copyright   : (c) 2020-2021 Tim Emiola
 SPDX-License-Identifier: BSD3
 Maintainer  : Tim Emiola <adetokunbo@users.noreply.github.com >
@@ -17,11 +18,10 @@ The instance this module provides can be used in integration tests as is.
 
 It's also possible to write other instances that launch @ZipKin@ in different
 ways; for those, this instance can be used as a reference example.
-
 -}
 module System.TmpProc.Docker.Zipkin
   ( -- * 'Proc' instance
-    TmpZipkin(..)
+    TmpZipkin (..)
 
     -- * Useful definitions
   , aProc
@@ -32,63 +32,77 @@ module System.TmpProc.Docker.Zipkin
   )
 where
 
-import           Control.Monad.IO.Class    (MonadIO, liftIO)
-import           Control.Monad.Trace.Class (MonadTrace, alwaysSampled, rootSpan)
-import qualified Data.ByteString.Char8     as C8
-import           Data.Proxy                (Proxy (..))
-import           Data.String               (fromString)
-import qualified Data.Text                 as Text
-import           Network.HTTP.Client       (HttpException)
-import           System.IO                 (Handle, IOMode (..), hPutStrLn,
-                                            openBinaryFile)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Trace.Class (MonadTrace, alwaysSampled, rootSpan)
+import qualified Data.ByteString.Char8 as C8
+import Data.Proxy (Proxy (..))
+import Data.String (fromString)
+import qualified Data.Text as Text
+import qualified Monitor.Tracing.Zipkin as ZPK
+import Network.HTTP.Client (HttpException)
+import System.IO
+  ( Handle
+  , IOMode (..)
+  , hPutStrLn
+  , openBinaryFile
+  )
+import System.TmpProc
+  ( Connectable (..)
+  , HList (..)
+  , HandlesOf
+  , HostIpAddress
+  , Pinged (..)
+  , Proc (..)
+  , ProcHandle (..)
+  , SvcURI
+  , startupAll
+  , toPinged
+  , withTmpConn
+  )
 
-import qualified Monitor.Tracing.Zipkin    as ZPK
 
-import           System.TmpProc            (Connectable (..), HList (..),
-                                            HandlesOf, HostIpAddress,
-                                            Pinged (..), Proc (..),
-                                            ProcHandle (..), SvcURI, startupAll,
-                                            toPinged, withTmpConn)
-
-
-{-| A singleton 'HList' containing a 'TmpZipkin'. -}
+-- | A singleton 'HList' containing a 'TmpZipkin'.
 aProc :: HList '[TmpZipkin]
 aProc = TmpZipkin `HCons` HNil
 
 
-{-| An 'HList' that just contains the handle created by 'aProc'. -}
+-- | An 'HList' that just contains the handle created by 'aProc'.
 aHandle :: IO (HandlesOf '[TmpZipkin])
 aHandle = startupAll aProc
 
 
-{-| Provides the capability to launch a Zipkin instance as a @tmp proc@. -}
+-- | Provides the capability to launch a Zipkin instance as a @tmp proc@.
 data TmpZipkin = TmpZipkin
 
 
-{-| Specifies how to run @ZipKin@ as a @tmp proc@. -}
+-- | Specifies how to run @ZipKin@ as a @tmp proc@.
 instance Proc TmpZipkin where
   type Image TmpZipkin = "openzipkin/zipkin-slim"
   type Name TmpZipkin = "a-zipkin-server"
 
+
   uriOf = mkUri'
   runArgs = []
+  pingCount = 6
+
 
   ping h = toPinged @HttpException Proxy $ do
     z <- openConn' h
     ZPK.run tracedPing z
     ZPK.publish z
 
+
   reset _ = pure ()
 
 
-{-| Specifies how to connect to a tmp @ZipKin@ service.
+{- | Specifies how to connect to a tmp @ZipKin@ service.
 
 In this case, there is not really a connection type, but 'ZPK.Zipkin' provides
 a close analogue.
-
 -}
 instance Connectable TmpZipkin where
   type Conn TmpZipkin = ZPK.Zipkin
+
 
   openConn = openConn'
   closeConn _ = pure ()
@@ -98,7 +112,7 @@ openConn' :: ProcHandle TmpZipkin -> IO ZPK.Zipkin
 openConn' = ZPK.new . toSettings
 
 
-{-| Make a simple HTTP uri to the zipkin server. -}
+-- | Make a simple HTTP uri to the zipkin server.
 mkUri' :: HostIpAddress -> SvcURI
 mkUri' ip = "http://" <> C8.pack (Text.unpack ip) <> "/"
 
@@ -116,4 +130,4 @@ tracedPing = rootSpan alwaysSampled "ping" $ liftIO pingAction
 
 
 devNull :: IO Handle
-devNull = openBinaryFile "/dev/null"  WriteMode
+devNull = openBinaryFile "/dev/null" WriteMode
