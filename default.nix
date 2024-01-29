fcec3d50ff5a339239f1c92317b542df6d10207f
Tim Emiola <adetokunbo@users.noreply.github.com>
2024-01-30 00:48:59 +0900

    Use dev.nix to simplify future nix dev updates

--- default.nix
+++ default.nix
@@ -1,17 +1,13 @@
 let
-  pkgsNix = import ./h8x.nix;
+  dev = import ./dev.nix;
+  pkgs-nix = import ./h8x.nix;
 in
-{ pkgs ? pkgsNix
+{ pkgs ? pkgs-nix
 } : pkgs.haskell-nix.cabalProject {
   # 'cleanGit' cleans a source directory based on the files known by git
   src = pkgs.haskell-nix.haskellLib.cleanGit {
     name = "tmp-proc";
     src = ./.;
   };
-
-  # Specify the GHC version to use.
-  compiler-nix-name = "ghc928";
-
-  # Specify the hackage index state
-  index-state = "2023-11-24T00:00:00Z";
+  inherit (dev) compiler-nix-name index-state;
 }
---
+++ dev.nix
@@ -0,0 +1,10 @@
+{
+  # Pin haskell-nix to a recent (as of 2023/11/24) commit
+  h8x-commit = "c6e3c91844e91f86cb64015258eed2ed8545d2a9";
+
+  # Specify the GHC version to use.
+  compiler-nix-name = "ghc928";
+
+  # Specify the hackage index state
+  index-state = "2023-11-24T00:00:00Z";
+}
