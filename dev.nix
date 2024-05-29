be22b8193db505ce89ff812f1145523c1cfd782b
Tim Emiola <adetokunbo@users.noreply.github.com>
2024-05-29 16:10:48 +0900

    Adjust the hackage pin

--- dev.nix
+++ dev.nix
@@ -3,12 +3,12 @@
   h8x-commit = "5297ad9e688b8a6d1ae0e8297f5502bccb5511a5";
 
   # Pin hackage-nix to the potentially more recent version of hackage
-  hackage-nix-commit = "25618a5293d11ef32e37bc10070783c3f720fa3a";
+  hackage-nix-commit = "a62dac66a95f2fb407c824592c6f325b8f7d0b60";
 
   # Specify the GHC version to use.
   compiler-nix-name = "ghc928";
 
   # Specify the hackage index state which should be supported by either
   # the h8x-commit, or the hackage-nix-commit, whichever is latest
-  index-state = "2024-05-12T00:00:00Z";
+  index-state = "2024-05-13T00:00:00Z";
 }
