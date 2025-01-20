5f73112edc8c46dcb07c2dc0d1111714f59a6b30
Tim Emiola <adetokunbo@users.noreply.github.com>
2025-01-20 14:42:07 +0900

    Use more recent ghc hlint and fourmolu

--- dev.nix
+++ dev.nix
@@ -6,7 +6,7 @@
   hackage-nix-commit = "88fb58dc1ccce1290b7db5773a867169374b539d";
 
   # Specify the GHC version to use.
-  compiler-nix-name = "ghc96";
+  compiler-nix-name = "ghc982";
 
   # Specify the hackage index state which should be supported by either
   # the h8x-commit, or the hackage-nix-commit, whichever is latest
