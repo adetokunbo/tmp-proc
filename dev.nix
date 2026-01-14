3f60fe4585ebe4e62aadea45e700909d446e5b0d
Tim Emiola <adetokunbo@users.noreply.github.com>
2026-01-14 15:28:13 +0900

    Update dev.nix and cabal.project to support HsOpenSSL

--- dev.nix
+++ dev.nix
@@ -1,14 +1,14 @@
 {
-  # Pin haskell-nix to a recent (as of 2024/12/29) commit
-  h8x-commit = "d65cc46ed1b8efadfbff6d41403b465525ffc225";
+  # Pin haskell-nix to a recent (as of 2025/06/21) commit
+  h8x-commit = "a3f435537829d97a1d2b1d27675963ac3ab21151";
 
   # Pin hackage-nix to the potentially more recent version of hackage
-  hackage-nix-commit = "88fb58dc1ccce1290b7db5773a867169374b539d";
+  hackage-nix-commit = "c4a5e33bcb9a1a605d4fb2a2f55ba85dd0965afc";
 
   # Specify the GHC version to use.
-  compiler-nix-name = "ghc982";
+  compiler-nix-name = "ghc98";
 
   # Specify the hackage index state which should be supported by either
   # the h8x-commit, or the hackage-nix-commit, whichever is latest
-  index-state = "2024-12-29T00:00:00Z";
+  index-state = "2025-06-20T00:00:00Z";
 }
